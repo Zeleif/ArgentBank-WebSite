f182e2cf691469c5f4176d6e7b70b8cad0b7d705
Zeleif <alyomeguja@gmail.com>
2023-11-21 14:03:19 +0100

    creation page (vide)

--- frontend/argentbank/src/App.js
+++ frontend/argentbank/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Homepage/Home";
+import SignIn from "./pages/SignIn/SignIn";
+import User from "./pages/User/User";
+import Error from "./pages/Error/Error";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/signin" element={<SignIn/>}/>
+          <Route path="/user" element={<User/>}/>
+          <Route path="*" element={<Error/>}/>
+        </Routes>
+    </BrowserRouter>
+    
   );
 }
-
-export default App;
+export default App
